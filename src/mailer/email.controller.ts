66b1e07df5bf61bfb5db0843f6f542a124319940
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2022-12-20 13:31:13 +0530

    Added file attachment feature to mailer.

--- src/mailer/email.controller.ts
+++ src/mailer/email.controller.ts
@@ -1,19 +1,12 @@
-import { MailerService } from '@nestjs-modules/mailer';
 import { Body, Controller, Post } from '@nestjs/common';
+import { EmailService } from './email.service';
 
 @Controller('email')
 export class EmailController {
-    constructor(private mailService:MailerService) {}
+    constructor(private readonly emailService: EmailService) { }
 
     @Post('sendMail')
-    async sendPlainTextMail(@Body() body: any) {
-        const response = await this.mailService.sendMail({
-            to: body.toMail,
-            from: 'mock.aadhar@gmail.com',
-            subject: body.subject,
-            text: body.content
-        })
-
-        return response;
+    sendMail(@Body() body: any) {
+        return this.emailService.sendMail(body.mailTo, body.subject, body.content, body.filename, body.filepath)
     }
 }
