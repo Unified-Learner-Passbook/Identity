3ab534af3db97777caad8f5cc45728e1f3e69e2f
Chinmoy Chakraborty <chinmoy12c@gmail.com>
2022-12-26 12:44:58 +0530

    Deleted mailer service.

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,32 +9,18 @@ import { HttpModule } from '@nestjs/axios';
 import { KycModule } from './kyc/kyc.module';
 import { ConfigModule } from '@nestjs/config';
 import { VcModule } from './vc/vc.module';
-import { EmailController } from './mailer/email.controller';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { EmailModule } from './mailer/email.module';
-import { EmailService } from './mailer/email.service';
 
 @Module({
   imports: [
     DidModule,
     KycModule,
     VcModule,
-    EmailModule,
     HttpModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    MailerModule.forRoot({
-      transport: {
-        host: process.env.EMAIL_HOST,
-        auth: {
-          user: process.env.EMAIL_SENDER,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      }
-    })
   ],
-  controllers: [AppController, DidController, EmailController],
-  providers: [AppService, PrismaService, DidService, EmailService],
+  controllers: [AppController, DidController],
+  providers: [AppService, PrismaService, DidService],
 })
 export class AppModule {}
